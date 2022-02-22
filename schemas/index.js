13d557d82fbc4c48ab5c3391416a367de59c983d
Ralph Largo <devlargs@gmail.com>
2022-02-23 02:22:49 +0800

    added queries for movies and directors

--- schemas/index.js
+++ schemas/index.js
@@ -1,13 +1,65 @@
 const graphql = require("graphql");
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLList,
+} = graphql;
+
+const directors = [
+  {
+    id: "dir_1",
+    name: "Martin Scorsese",
+  },
+  {
+    id: "dir_2",
+    name: "Christopher Nolan",
+  },
+];
+
+const movies = [
+  {
+    id: "mov_1",
+    name: "Shutter Island",
+    genre: "Fiction",
+    directorId: `dir_1`,
+  },
+  {
+    id: "mov_2",
+    name: "The Dark Knight",
+    genre: "Fiction",
+    directorId: `dir_2`,
+  },
+];
 
 const MovieType = new GraphQLObjectType({
   name: "Movie",
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+    director: {
+      type: DirectorType,
+      resolve: (parent, args) => {
+        return directors.filter((q) => q.id === parent.directorId)?.[0] ?? null;
+      },
+    },
+  }),
+});
+
+const DirectorType = new GraphQLObjectType({
+  name: "Director",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    movies: {
+      type: new GraphQLList(MovieType),
+      resolve(parent) {
+        return movies.filter((q) => q.directorId === parent.id);
+      },
+    },
   }),
 });
 
@@ -18,17 +70,22 @@ const RootQuery = new GraphQLObjectType({
       type: MovieType,
       args: {
         id: {
-          type: GraphQLString,
+          type: GraphQLID,
         },
       },
-      resolve(parent, args) {
-        return [
-          {
-            id: "1",
-            name: "Fear and Loathing from las vegas",
-            genre: "Fiction",
-          },
-        ].filter((q) => q.id === args.id)[0];
+      resolve(_, args) {
+        return movies.filter((q) => q.id === args.id)[0];
+      },
+    },
+    director: {
+      type: DirectorType,
+      args: {
+        id: {
+          type: GraphQLID,
+        },
+      },
+      resolve(_, args) {
+        return directors.filter((q) => q.id === args.id)[0];
       },
     },
   },
