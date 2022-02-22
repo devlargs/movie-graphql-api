b648b2eb66e4708101271f07375c791dff9ec565
Ralph Largo <devlargs@gmail.com>
2022-02-23 04:24:06 +0800

    added branch bumping

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const mongoose = require("mongoose");
 const schema = require("./schemas/index");
+const { version } = require("./package.json");
 
 const app = express();
 const PORT = 3000;
@@ -23,6 +24,15 @@ app.use(
     graphiql: true,
   })
 );
+
+app.use("/", (req, res) => {
+  res.send({
+    title: "Movies Graphql API",
+    createdBy: "Ralph Largo",
+    purpose: "Testing",
+    version,
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Listening to port ${PORT}`);
